2f4dc330563bea60b32fc76579ec35b84f250576
AGRamirezz94 <aramirez64@ucmerced.edu>
2017-02-26 14:40:56 -0800

    Edits
    Edited functions to return strings then print

--- formgrammarass.py
+++ formgrammarass.py
@@ -7,17 +7,17 @@ N = [' car ', ' tree ', ' whale ', ' pie ', ' dog ', ' phone ']
 A = 'the '
 
 #Verbs
-V = [' ran ', ' hit ', ' smashed ', ' grazed ']
+V = ['ran ', 'hit ', 'smashed ', 'grazed ']
 
 #Noun phrase = <Article>, <Noun>
 def get_np():
-    print A + random.choice(N)
+    return A + random.choice(N)
 
 #Predicate = <Verb>, <Article>, <Direct Object>
 def get_p():
-    print random.choice(V) + A + random.choice(N)
+    return random.choice(V) + A + random.choice(N)
     
 #print <Noun phrase> <Predicate> example
 for i in range (1, 10):
-    get_np() , get_p()
+    print get_np(), get_p()
 
